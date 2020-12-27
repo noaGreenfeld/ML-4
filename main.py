5ab24eec337a4d7987015921de3b74aa996a8a89
green <green@LAPTOP-SR93CP99>
2020-12-27 14:05:20 +0200

    a c e model

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ from torchvision import transforms as tr
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets
 
+
+# todo change name of function and remove redundant print
 def print_hi(name):
     print(f'Hi, {name}')
     train_loader, val_loader = load()
-    #modela
+    #modelA
+    print("************A**************")
     model = ModelA(28*28)
     optimizer = optim.SGD(model.parameters(), lr=0.01)
+    for e in range(10):
+        print(e)
+        train(model, optimizer, train_loader)
+        validate(model, val_loader)
+    #modelC
+    print("************C**************")
+    model = ModelC(28*28)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    for e in range(10):
+        print(e)
+        train(model, optimizer, train_loader)
+        validate(model, val_loader)
+    #modelE
+    print("************E**************")
+    model = ModelE(28*28)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     for e in range(10):
         print(e)
         train(model, optimizer, train_loader)
@@ -60,34 +79,33 @@ def train(model, optimizer, train_loader):
         train_loss, correct, len(train_loader.dataset),
         100. * correct / len(train_loader.dataset)))
 
-
-
 def load():
     train_x = np.loadtxt("train_x")
     train_y = np.loadtxt("train_y")
-    #split train file to validation and train:
+    # split train file to validation and train:
     size_train = int(len(train_x) * 0.2)
     validation_x = train_x[-size_train:, :]
     validation_y = train_y[-size_train:]
     train_x = train_x[: -size_train, :]
     train_y = train_y[: -size_train]
 
-    #todo - check the avg and change it
-    transforms= tr.Compose([tr.ToTensor(),
+    # todo - check the avg and change it
+    transforms = tr.Compose([tr.ToTensor(),
                             tr.Normalize((0.1307,), (0.3081,))])
-    #train data
-    train_data= FashionData(train_x, train_y, transforms)
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle= True)
-    #validation data
-    validation_data= FashionData(validation_x, validation_y, transforms)
-    val_loader = torch.utils.data.DataLoader(validation_data,shuffle= True)
+    # train data
+    train_data = FashionData(train_x, train_y, transforms)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    # validation data
+    validation_data = FashionData(validation_x, validation_y, transforms)
+    val_loader = torch.utils.data.DataLoader(validation_data, shuffle=True)
     return train_loader, val_loader
+
 
 class FashionData(Dataset):
     def __init__(self, x, y, transforms):
-        self.x=x
-        self.y=y
-        self.transforms= transforms
+        self.x = x
+        self.y = y
+        self.transforms = transforms
 
     def __len__(self):
         return len(self.x)
@@ -114,12 +132,12 @@ class ModelA(nn.Module):
         x = x.view(-1, self.image_size)
         x = F.relu(self.fc0(x))
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = (self.fc2(x))
         return F.log_softmax(x, dim=1)
 
 class ModelB(nn.Module):
     def __init__(self, image_size):
-        super(ModelA, self).__init__()
+        super(ModelB, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 100)
         self.fc1 = nn.Linear(100, 50)
@@ -131,6 +149,45 @@ class ModelB(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return F.log_softmax(x, dim=1)
+
+class ModelC(nn.Module):
+    def __init__(self, image_size):
+        super(ModelC, self).__init__()
+        self.image_size = image_size
+        self.fc0 = nn.Linear(image_size, 100)
+        self.fc1 = nn.Linear(100, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.drop_layer = nn.Dropout(p=0.6)
+
+    def forward(self, x):
+        x = x.view(-1, self.image_size)
+        x = F.relu(self.fc0(x))
+        x = F.relu(self.fc1(x))
+        x = (self.fc2(x))
+        self.drop_layer(x)
+        return F.log_softmax(x, dim=1)
+
+class ModelE(nn.Module):
+    def __init__(self, image_size):
+        super(ModelE, self).__init__()
+        self.image_size = image_size
+        self.fc0 = nn.Linear(image_size, 128)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, 10)
+        self.fc5 = nn.Linear(10, 10)
+
+
+    def forward(self, x):
+        x = x.view(-1, self.image_size)
+        x = F.relu(self.fc0(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = (self.fc5(x))
+        return F.log_softmax(x, dim=1)
+
 if __name__ == '__main__':
     print_hi('noaaaaa')
-
